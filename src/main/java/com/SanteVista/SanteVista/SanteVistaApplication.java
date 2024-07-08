bce0b75875edf37e7f2ea2fbcba4f04df29ee25c
AzaiezAbdessalem <azaiezabdessalem@gmail.com>
2024-07-09 00:07:10 +0200

    first

---
+++ src/main/java/com/SanteVista/SanteVista/SanteVistaApplication.java
@@ -0,0 +1,13 @@
+package com.SanteVista.SanteVista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanteVistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanteVistaApplication.class, args);
+	}
+
+}
